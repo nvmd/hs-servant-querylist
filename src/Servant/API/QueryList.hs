8cfb69d5430128d8c9c8f921fe7491859a283131
Sergey Kazenyuk <kazenyuk@gmail.com>
2017-05-26 03:33:08 +0300

    Move Servant QueryList snippet from swagger-codegen to a separate package
    It seems to be useful beyond the swagger-codegen haskell-servant
    code generator library.
    All copyright notices and licenses are preserved to be intact with
    library origin and subsequent contributions by other authors.

---
+++ src/Servant/API/QueryList.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Servant.API.QueryList where
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import Web.HttpApiData
+
+
+-- | List of elements parsed from a query.
+newtype QueryList (p :: CollectionFormat) a = QueryList
+  { fromQueryList :: [a]
+  } deriving (Functor, Applicative, Monad, Foldable, Traversable)
+
+-- | Formats in which a list can be encoded into a HTTP path.
+data CollectionFormat
+  = CommaSeparated -- ^ CSV format for multiple parameters.
+  | SpaceSeparated -- ^ Also called "SSV"
+  | TabSeparated -- ^ Also called "TSV"
+  | PipeSeparated -- ^ `value1|value2|value2`
+  | MultiParamArray -- ^ Using multiple GET parameters, e.g. `foo=bar&foo=baz`. Only for GET params.
+
+instance FromHttpApiData a => FromHttpApiData (QueryList 'CommaSeparated a) where
+  parseQueryParam = parseSeparatedQueryList ','
+
+instance FromHttpApiData a => FromHttpApiData (QueryList 'TabSeparated a) where
+  parseQueryParam = parseSeparatedQueryList '\t'
+
+instance FromHttpApiData a => FromHttpApiData (QueryList 'SpaceSeparated a) where
+  parseQueryParam = parseSeparatedQueryList ' '
+
+instance FromHttpApiData a => FromHttpApiData (QueryList 'PipeSeparated a) where
+  parseQueryParam = parseSeparatedQueryList '|'
+
+instance FromHttpApiData a => FromHttpApiData (QueryList 'MultiParamArray a) where
+  parseQueryParam = error "unimplemented FromHttpApiData for MultiParamArray collection format"
+
+parseSeparatedQueryList :: FromHttpApiData a => Char -> Text -> Either Text (QueryList p a)
+parseSeparatedQueryList char = fmap QueryList . mapM parseQueryParam . T.split (== char)
+
+instance ToHttpApiData a => ToHttpApiData (QueryList 'CommaSeparated a) where
+  toQueryParam = formatSeparatedQueryList ','
+
+instance ToHttpApiData a => ToHttpApiData (QueryList 'TabSeparated a) where
+  toQueryParam = formatSeparatedQueryList '\t'
+
+instance ToHttpApiData a => ToHttpApiData (QueryList 'SpaceSeparated a) where
+  toQueryParam = formatSeparatedQueryList ' '
+
+instance ToHttpApiData a => ToHttpApiData (QueryList 'PipeSeparated a) where
+  toQueryParam = formatSeparatedQueryList '|'
+
+instance ToHttpApiData a => ToHttpApiData (QueryList 'MultiParamArray a) where
+  toQueryParam = error "unimplemented ToHttpApiData for MultiParamArray collection format"
+
+formatSeparatedQueryList :: ToHttpApiData a => Char ->  QueryList p a -> Text
+formatSeparatedQueryList char = T.intercalate (T.singleton char) . map toQueryParam . fromQueryList
